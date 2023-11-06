51885fa5a6ae8f2034ca9bec29f6add654e932da
Jenke Scheen <jenke.scheen@choderalab.org>
2023-11-06 10:49:09 +0100

    base index page render

--- website/website/settings.py
+++ website/website/settings.py
@@ -76,7 +76,8 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    # might need to change this once we hook up to ec2
+    'default': { 
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
