0e0d13d67978325f2fccceda4233ec2f0b0fd07c
hmacdope <hugomacdermott@gmail.com>
2024-02-08 16:50:22 +1100

    try get initial tests working

--- argos/argos_viewer/tests.py
+++ argos/argos_viewer/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class BlankTest(TestCase):
+    def test_placeholder(self):
+        self.assertEqual(1, 1)
+
+    def test_fail(self):
+        self.assertEqual(1,2)
