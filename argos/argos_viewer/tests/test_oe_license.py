5584f073c6db4ae17d9a899f0501df530fb5eae6
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2024-02-29 12:03:13 +1100

    Update test_oe_license.py

--- argos/argos_viewer/tests/test_oe_license.py
+++ argos/argos_viewer/tests/test_oe_license.py
@@ -2,5 +2,5 @@ from django.test import TestCase
 
 class OELicenseTests(TestCase):
     def test_oechem_is_licensed(self):
-        from asapdiscovery.data.openeye import oechem
+        from asapdiscovery.data.backend.openeye import oechem
         self.assertTrue(oechem.OEChemIsLicensed())
